91f2812d6c3016e225113d4917ed30c4b86bbdc6
Dushyant Juneja <juneja.dushyant@gmail.com>
2022-05-01 10:32:10 -0400

    Add functionality using IntersectionObserver API

--- js/app.js
+++ js/app.js
@@ -17,51 +17,100 @@
  * Define Global Variables
  * 
 */
-const $ = document.querySelector;
-const $$ = document.querySelectorAll;
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+const devMode = true;
+const intersectionOptions = {
+    root: null,
+    rootMargin: "10px",
+    threshold: 1.0
+};
+
+const intersectionCallback = (items, _) => {
+    const sectionArea = items[0];
+    if (sectionArea.isIntersecting) {
+        const section = sectionArea.target;
+        activateOnlySection(section);
+        activateInNavbar(section);
+    }
+}
+
+const observer = new IntersectionObserver(
+    intersectionCallback, intersectionOptions
+)
 
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
+const _devMsg = x => devMode ?
+    console.log(":: DEV MODE ::" + x)
+    : undefined
 
+const activateInNavbar = section => {
+    const sectionsList = $$(`#navbar__list li`);
+    sectionsList.forEach(item => 
+        item.style.backgroundColor = item.dataset.nav.substring(1) === section.id ?
+            "yellow" : "");
+}
 
+const activateOnlySection = thisSection =>
+    $$("section").forEach(section => {
+        section.id !== thisSection.id ?
+            section.classList.remove("my-active-class") :
+            section.classList.add("my-active-class")
+    });
 
+const addEventsToNavbar = _ => {
+    $$("#navbar__list li").forEach(item =>
+        item.addEventListener("click", e => {
+            const targetId = e.target.dataset.nav;
+            _devMsg(targetId);
+
+            const targetSection = $(e.target.dataset.nav);
+
+            // Verbatim from Examples at Mozilla
+            // https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollIntoView
+            targetSection
+                .scrollIntoView({
+                    behavior: "smooth", 
+                    block: "end", 
+                    inline: "nearest"
+                })
+
+            // Remove "active" class from others
+            // And add active where needed is taken care by intersection observer
+    }));
+}
 /**
  * End Helper Functions
  * Begin Main Functions
  * 
 */
 
-// build the nav
-$$("section").forEach(section => {
-    const sel = `#${section.id} div h2`;
+// Set the ball rolling when things are ready!
+document.addEventListener("DOMContentLoaded", _ => {
+    // build the nav
+    $$("section").forEach(section => {
+        const sel = `#${section.id} div h2`;
+    
+        // There are better ways out there.
+        // This works for now -- we just have 4 elements.
+        $("#navbar__list").innerHTML +=
+            `<li data-nav="#${section.id}">
+                ${$(sel).textContent}
+            </li>`;
+        
+        observer.observe(section);
+    });
 
-    // There are better ways out there.
-    // This works for now.
-    $("#navbar__list").innerHTML +=
-        `<li id="nav_${section.id}">
-            ${$(sel).textContent}
-        </li>`;
-});
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
+    // Scroll to anchor ID using scrollTO event
+    addEventsToNavbar();
+})
 
 
 /**
  * End Main Functions
- * Begin Events
  * 
 */
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
-
-
